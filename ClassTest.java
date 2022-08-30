7402f15a2478d2f52033d4abddcb38fa516c7c8c
VladimirD777 <112504091+VladimirD777@users.noreply.github.com>
2022-08-30 20:41:19 +0300

    Add files via upload

---
+++ ClassTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class ClassTest {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 3) {
+            System.out.println("123");
+            a = a + 1;
+        }
+    }
+}
